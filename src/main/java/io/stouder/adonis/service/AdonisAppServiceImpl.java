b63ac9edc7746ce4c5eabfb007536d4d80ec10b3
Xavier Stouder <xavier@stouder.io>
2023-11-13 21:40:45 +0100

    chore: add util function in app service

--- src/main/java/io/stouder/adonis/service/AdonisAppServiceImpl.java
+++ src/main/java/io/stouder/adonis/service/AdonisAppServiceImpl.java
@@ -2,9 +2,14 @@ package io.stouder.adonis.service;
 
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.VirtualFile;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AdonisAppServiceImpl implements AdonisAppService {
     private static final Logger LOG = Logger.getInstance(AdonisAppService.class);
@@ -21,6 +26,20 @@ public class AdonisAppServiceImpl implements AdonisAppService {
             return false;
         }
         String adonisrcFilePath = FileUtil.toSystemIndependentName(projectBasePath + "/adonisrc.ts");
+        File adonisrcFile = new File(adonisrcFilePath);
+        return adonisrcFile.exists();
+    }
+
+    public List<String> getAdonisRoots() {
+        VirtualFile[] modulesRoots = ProjectRootManager.getInstance(project).getContentRoots();
+        return Arrays.stream(modulesRoots)
+                .map(VirtualFile::getCanonicalPath)
+                .filter(this::isAdonisModule)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isAdonisModule(String basePath) {
+        String adonisrcFilePath = FileUtil.toSystemIndependentName(basePath + "/adonisrc.ts");
         File adonisrcFile = new File(adonisrcFilePath);
         return adonisrcFile.exists();
     }
