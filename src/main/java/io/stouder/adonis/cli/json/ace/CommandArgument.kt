ee110a4478ecbd275acf5965b3e3cc845d6a7614
Prisca <prisca.castanier@gmail.com>
2024-05-04 20:42:12 +0200

    chore: update all ace commands related methods to kt

---
+++ src/main/java/io/stouder/adonis/cli/json/ace/CommandArgument.kt
@@ -0,0 +1,9 @@
+package io.stouder.adonis.cli.json.ace
+
+data class CommandArgument(
+    val name: String,
+    val argumentName: String,
+    val description: String,
+    val required: Boolean,
+    val type: String
+)
