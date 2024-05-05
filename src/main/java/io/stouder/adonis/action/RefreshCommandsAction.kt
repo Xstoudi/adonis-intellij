53182b6f8d27ba97ac71c31df3babe2b6826103d
Prisca <prisca.castanier@gmail.com>
2024-05-05 02:37:02 +0200

    fix: fix issue with async method not running in the right thread when refreshing routes / commands

--- src/main/java/io/stouder/adonis/action/RefreshCommandsAction.kt
+++ src/main/java/io/stouder/adonis/action/RefreshCommandsAction.kt
@@ -2,25 +2,31 @@ package io.stouder.adonis.action
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
 import io.stouder.adonis.AdonisBundle
 import io.stouder.adonis.cli.json.ace.Command
 import io.stouder.adonis.notifier.AdonisRcUpdateNotifier
 import io.stouder.adonis.service.AdonisAceService
 import org.jetbrains.annotations.NotNull
+import java.util.*
 
 class RefreshCommandsAction : AnAction() {
 
     override fun actionPerformed(@NotNull e: AnActionEvent) {
-        val project = e.project ?: return
+    val project = e.project ?: return
 
-        e.presentation.isEnabled = false
-        val publisher = project.messageBus.syncPublisher(AdonisRcUpdateNotifier.ADONIS_RC_UPDATE_TOPIC)
+    e.presentation.isEnabled = false
+    val publisher = project.messageBus.syncPublisher(AdonisRcUpdateNotifier.ADONIS_RC_UPDATE_TOPIC)
+
+    ApplicationManager.getApplication().executeOnPooledThread {
         val commands = AdonisAceService.getInstance(project).runAceGetCommandOnEveryRoots(
             AdonisBundle.message("adonis.actions.refresh.commands"),
             listOf("list", "--json"),
             Array<Command>::class.java
         )
-        publisher.commands(commands)
-        e.presentation.isEnabled = true
+
+        val safeCommands = commands as? Map<String, Optional<Array<Command>>>
+        safeCommands?.let { publisher.commands(it) }
     }
+}
 }
--- src/main/java/io/stouder/adonis/action/RefreshRoutesAction.kt
+++ src/main/java/io/stouder/adonis/action/RefreshRoutesAction.kt
@@ -2,29 +2,38 @@ package io.stouder.adonis.action
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
 import io.stouder.adonis.AdonisBundle
 import io.stouder.adonis.cli.json.routes.RouteDomain
 import io.stouder.adonis.notifier.AdonisRouteUpdateNotifier
 import io.stouder.adonis.service.AdonisAceService
+import java.util.*
 
 class RefreshRoutesAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
-        val project = e.project
-        assert(project != null)
+    val project = e.project
+    assert(project != null)
 
-        e.presentation.isEnabled = false
-        val routeDomains = project?.let {
+    e.presentation.isEnabled = false
+
+    ApplicationManager.getApplication().executeOnPooledThread {
+        val routeDomainsFuture = project?.let {
             AdonisAceService.getInstance(it).runAceGetCommandOnEveryRoots(
                 AdonisBundle.message("adonis.actions.refresh.routes"),
                 listOf("list:routes", "--json"),
                 Array<RouteDomain>::class.java
             )
         }
-        project
-            ?.messageBus
-            ?.syncPublisher(AdonisRouteUpdateNotifier.ADONIS_ROUTES_UPDATE_TOPIC)
-            ?.routes(routeDomains)
-        e.presentation.isEnabled = true
+
+        ApplicationManager.getApplication().invokeLater {
+            val routeDomains = routeDomainsFuture
+            project
+                ?.messageBus
+                ?.syncPublisher(AdonisRouteUpdateNotifier.ADONIS_ROUTES_UPDATE_TOPIC)
+                ?.routes(routeDomains)
+            e.presentation.isEnabled = true
+        }
     }
+}
 }
