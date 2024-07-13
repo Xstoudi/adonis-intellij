fb0a287b1ba51498ba50e23c73cbf7c67dff3095
Prisca <prisca.castanier@gmail.com>
2024-07-13 19:07:20 +0200

    chore: update EdgeEmmetGenerator to kt

---
+++ src/main/java/io/stouder/adonis/edge/editor/templates/EdgeEmmetGenerator.kt
@@ -0,0 +1,19 @@
+package io.stouder.adonis.edge.editor.templates
+
+import com.intellij.codeInsight.template.emmet.generators.XmlZenCodingGeneratorImpl
+import com.intellij.lang.Language
+import io.stouder.adonis.edge.EdgeLanguage
+import com.intellij.openapi.options.Configurable
+import org.jetbrains.annotations.Nullable
+
+class EdgeEmmetGenerator : XmlZenCodingGeneratorImpl() {
+
+    @Nullable
+    override fun createConfigurable(): Configurable? {
+        return super.createConfigurable()
+    }
+
+    override fun isMyLanguage(language: Language): Boolean {
+        return language.isKindOf(EdgeLanguage.INSTANCE)
+    }
+}
