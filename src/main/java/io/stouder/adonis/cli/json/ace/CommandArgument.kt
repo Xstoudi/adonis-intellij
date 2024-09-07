0095a852e16727aa7706d6abe22d47788f571f91
Xavier Stouder <xavier@stouder.io>
2024-09-07 22:44:36 +0200

    fix: command argument description may be null

--- src/main/java/io/stouder/adonis/cli/json/ace/CommandArgument.kt
+++ src/main/java/io/stouder/adonis/cli/json/ace/CommandArgument.kt
@@ -3,7 +3,7 @@ package io.stouder.adonis.cli.json.ace
 data class CommandArgument(
     val name: String,
     val argumentName: String,
-    val description: String,
+    val description: String?,
     val required: Boolean,
     val type: String
 )
