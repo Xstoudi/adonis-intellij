ec2593e8faa69115b19952c00f4a8575e12508e4
Xavier Stouder <xavier@stouder.io>
2024-09-08 02:24:48 +0200

    chore: remove commends in gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,6 @@ repositories {
 
 sourceSets["main"].java.srcDirs("src/main/gen")
 
-
 dependencies {
   compileOnly("org.projectlombok:lombok:1.18.30")
   annotationProcessor("org.projectlombok:lombok:1.18.30")
@@ -68,9 +67,6 @@ intellijPlatform {
 
   publishing {
     token = providers.environmentVariable("PUBLISH_TOKEN")
-    // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
-    // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-    // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
     channels = providers.gradleProperty("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
   }
 
@@ -147,9 +143,6 @@ tasks {
   publishPlugin {
     dependsOn("patchChangelog")
     token = environment("PUBLISH_TOKEN")
-    // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
-    // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-    // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
     channels = properties("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
   }
 }
