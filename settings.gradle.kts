d18c91eb63c874b607af6998b3a734bd15e32433
Xavier Stouder <xavier@stouder.io>
2024-09-08 21:42:43 +0200

    fix: use gradle 8.10

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,1 @@
-//pluginManagement {
-//    plugins {
-//        kotlin("jvm") version "1.9.23"
-//    }
-//}
-//plugins {
-//    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
-//}
-
 rootProject.name = "adonis"
